28c46ee49ad89a9bc6733e1d0b5d97c3fdcde0c2
archdukeGonzi <gonzalojrnunez@icloud.com>
2024-09-11 12:42:33 -0700

    Just pushing the blank project

---
+++ A Safer San Diego/A Safer San Diego/A_Safer_San_DiegoApp.swift
@@ -0,0 +1,17 @@
+//
+//  A_Safer_San_DiegoApp.swift
+//  A Safer San Diego
+//
+//  Created by Gonzalo Jr Nunez on 9/11/24.
+//
+
+import SwiftUI
+
+@main
+struct A_Safer_San_DiegoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ A Safer San Diego/A Safer San Diego/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  A Safer San Diego
+//
+//  Created by Gonzalo Jr Nunez on 9/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
