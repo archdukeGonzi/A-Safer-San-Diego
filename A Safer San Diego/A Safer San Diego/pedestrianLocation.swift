7cf5c6fe876778010ae94c6243133211ef35c33f
archdukeGonzi <gonzalojrnunez@icloud.com>
2024-09-19 15:49:51 -0700

    User location permissions and error handling

---
+++ A Safer San Diego/A Safer San Diego/pedestrianLocation.swift
@@ -0,0 +1,8 @@
+//
+//  pedestrianLocation.swift
+//  A Safer San Diego
+//
+//  Created by Gonzalo Jr Nunez on 9/11/24.
+//
+
+import Foundation
---
+++ A Safer San Diego/A Safer San Diego/userMapLocation.swift
@@ -0,0 +1,8 @@
+//
+//  userMapLocation.swift
+//  A Safer San Diego
+//
+//  Created by Gonzalo Jr Nunez on 9/19/24.
+//
+
+import Foundation
